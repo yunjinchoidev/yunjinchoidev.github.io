ed620832dfccea7cabba2c2b2fce2e7a4c8915a0
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-08 01:20:56 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": null,
-        "excerpt":"This is my First Post     this is my first report   ","categories": [],
+        "excerpt":"This is my First Post this is my first report 중앙 정렬입니다. 원하는 이미지를 원하는 위치에서 표출할 수 있는 예시입니다. 해당 출처는 minimal mistakes의 공식 사이트에 나와있는 글을 참조했습니다. 그 중에서 제가 자주 사용하는 내용만 가져왔습니다. Primary 문자 박스입니다. 문자 박스는 색이 달라 원하는 정보를 블로그 방문자에게 보여주기 좋은 것...","categories": [],
         "tags": null,
-        "url": "/2023-03-07-post-my/",
+        "url": "/2023-03-07-post-day01/",
+        "teaser": null
+      },{
+        "title": null,
+        "excerpt":"This is my First Post this is my first report 중앙 정렬입니다. 원하는 이미지를 원하는 위치에서 표출할 수 있는 예시입니다. 해당 출처는 minimal mistakes의 공식 사이트에 나와있는 글을 참조했습니다. 그 중에서 제가 자주 사용하는 내용만 가져왔습니다. Primary 문자 박스입니다. 문자 박스는 색이 달라 원하는 정보를 블로그 방문자에게 보여주기 좋은 것...","categories": [],
+        "tags": null,
+        "url": "/2023-03-07-post-day02/",
         "teaser": null
       }]
