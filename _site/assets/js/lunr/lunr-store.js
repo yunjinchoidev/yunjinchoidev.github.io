9d53a9d7aa78a5b6d07ab46b3194d1a4f43f22b6
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-07 23:47:31 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": null,
+        "excerpt":"This is my First Post     this is my first report   ","categories": [],
+        "tags": null,
+        "url": "/2023-03-07-post-my/",
+        "teaser": null
+      }]
