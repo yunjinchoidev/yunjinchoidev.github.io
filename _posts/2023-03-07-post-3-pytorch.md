4dca40fd1d81c4c6041c7f497d1aff293bd9de4a
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-08 02:01:07 +0900

    post

--- _posts/2023-03-07-post-3-pytorch.md
+++ _posts/2023-03-07-post-3-pytorch.md
@@ -10,3 +10,160 @@ tages:
 
 # boostcamp_AITech_5th
 ![image]("../../../image/aitech.png)
+
+
+# [Pytorch] 강의록
+
+---
+# 키워드
+* 네트워크 구현
+* 데이터 로딩
+* 프로젝트 구조 잡기
+* 로깅
+* Multi GPU
+
+
+---
+# 1.  Introduction to PyTorch
+- 밑바닥부터 딥러닝 코드 짜기? [책](https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=256067157&start=slayer)
+- 딥러닝 프레임워크
+  - 텐서플로
+  - 파이토치 
+- 우리는 파이토치를 쓰자.
+- 계산 그래프 (Computational Graph)
+  - Define and Rune
+    - 그래프를 먼저 정의
+  - Define by Run
+    - 실행을 하면서 그래프를 생성
+- Pytorch
+  - 학회, 논문에 강점
+  - **즉시 확인 가능**
+  - **Numpy + AutoGrad + Function **
+  - Multi GPU
+- Tensorflow
+  - 프로덕션에 강점
+
+
+# 2. PyTorch Basics
+- AutoGrad
+```
+w = torch.tensor(2.0, requires_grad=True)
+y = w**2
+z = 10*y + 25
+z.backward()
+w.grad
+
+> z.backward() 를 통해 오차역전파를 계산
+> w.grad 를 통해 z 에 대한 w 그레디언트 추출
+```
+
+- 최대가능도 추정법
+  - 모수
+    - 모수는 모집단의 특성(모평균,모분산 등..)을 나타내는 값으로, 이 값을 모집단을 전수조사해야만 알수있는 값이다. 그러나 실질적으로 모집단의 크기와 범위가 너무 방대하기에 전수조사를 실지하지 않고 표본조사를 하는데 표본평균,표본분산 등으로 모평균, 모분산등을 추정할수가 있다.
+
+
+
+
+
+
+
+
+# 3. Project Structure
+- 언제까지 코랩쓸래? 
+- 템플릿을 쓰자.
+- 템플릿
+  - 실행, 데이터, 모델,  설정, 로깅, 지표, 유틸리티
+  - [링크](https://github.com/victoresque/pytorch-template)
+  - **템플릿 구조를 면밀하게 분석하는 것을 해보시라.**
+
+
+---
+# 4. AutoGrad & Optimizer
+- 논문을 구현해봅시다.
+- 딥러닝 -> layer 쌓기 -> 레고 블럭 쌓기
+- 4요소
+  - input
+  - output
+  - forward
+  - backward
+
+
+
+
+---
+
+# 5. Pytorch Dataset & Dataloader
+- 모델도 중요하지만, 데이터를 어떻게 잘 다루느냐도 중요하다.
+  - 엄청나게 많은 데이터
+- **Pytorch Dataset**
+  - 먼저 데이터를 텐서로 바꾸고 DataLoder 로 모델에 먹이기
+- **CustomDataset**
+  - CustomDataset을 정의 하기 위해서 3개의 메소드를 반드시 정의해야 한다.
+    - init, len, getitem
+    - utils.data.Dataset 상속
+- DataLoder
+  - CustomDataset, 기타 옵션으로 실제 데이터에 적용하면 된다.
+  - 옵션
+    - 미니배치 사이즈
+    - ... 많다.
+  - 코드를 뜯어보시라.
+
+
+---
+# 6. 모델 불러오기
+- 상황
+  - 이 말인 즉슨 우리는 실제 라벨과 예측 라벨의 오차 함수의 그레디언트를 계산하고 오차역전파를 계산하여 W 가중치 텐서를 업데이트 시켰다. (공부를 했다. 경험을 했다. 수련을 했다.)
+  - 우리는 이 결과를 전달하려 한다.
+- model.save() 
+- 저장법 2가지
+  - 파라메터만 저장
+  - 모델형테 + 파라메터 저장
+
+
+
+
+---
+# 7. Monitoring tools for PyTorch
+- 체크포인트
+  - 학습의 중간 결과를 저장
+  - earlystopping 
+  - loss, metric
+  - Earlystopping : 조기 종료
+
+- Loggin
+  - Logging이란, 학습이 진행되면서 주요하다고 생각되는 지표들이나 학습의 상태 등 관련된 정보들을 기록하는 것
+
+- Tensorboard
+- Tensorboard는 TensorFlow의 프로젝트로 만들어진 시각화 도구로 학습 그래프, Metric, 학습 결과의 시각화를 지원
+
+- Weight, Biases
+  - 머신러닝 실험을 원활하게 지원하기 위한 상용 도구로써, 협업, code versioning, 실험 결과 기록 등의 기능을 제공하는 시각화 툴
+---
+# 8. Muti-GPU
+
+
+
+
+---
+
+# 9. Hyperparameter Tuning
+- 학습률(Learning rate)
+- 최적화 함수(Optimizer)
+- 손실 함수(Loss Fucnction)
+-  
+
+
+---
+
+# 10. PyTorch Troubleshooting
+
+
+
+
+---
+
+
+---
+# 생각해볼점
+- AutoGrad 작동 방식 ? 
+- 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,43 @@
 var store = [{
-        "title": null,
-        "excerpt":"This is my First Post this is my first report 중앙 정렬입니다. 원하는 이미지를 원하는 위치에서 표출할 수 있는 예시입니다. 해당 출처는 minimal mistakes의 공식 사이트에 나와있는 글을 참조했습니다. 그 중에서 제가 자주 사용하는 내용만 가져왔습니다. Primary 문자 박스입니다. 문자 박스는 색이 달라 원하는 정보를 블로그 방문자에게 보여주기 좋은 것...","categories": [],
-        "tags": null,
-        "url": "/2023-03-07-post-day01/",
+        "title": "Post: ai tech 온보딩",
+        "excerpt":"boostcamp_AITech_5th (23.3.1 ~ 23.3.5) [온보딩기간을 보내며] 3월 1일 부터 3월 5일까지 온보딩 기간을 보내며 설레는 마음으로 공부를 했다. 파이썬, ai math, pytorch 강의를 들었다. 수학과를 다니면서 선형대수학을 과연 사용할 일이 있을까 생각을 했었는데 딥러닝에서 매우 중요하다고 한다. 통계학도 중요하다. 그래서 선형대수, 수리통계 책을 구입했다. 8월 2일까지 진행한다. 나는 nlp 트랙으로...","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-%EC%98%A8%EB%B3%B4%EB%94%A9/",
         "teaser": null
       },{
-        "title": null,
-        "excerpt":"This is my First Post this is my first report 중앙 정렬입니다. 원하는 이미지를 원하는 위치에서 표출할 수 있는 예시입니다. 해당 출처는 minimal mistakes의 공식 사이트에 나와있는 글을 참조했습니다. 그 중에서 제가 자주 사용하는 내용만 가져왔습니다. Primary 문자 박스입니다. 문자 박스는 색이 달라 원하는 정보를 블로그 방문자에게 보여주기 좋은 것...","categories": [],
-        "tags": null,
-        "url": "/2023-03-07-post-day02/",
+        "title": "Post: ai tech 1주차",
+        "excerpt":"boostcamp_AITech_5th     (23.3.6 ~ 23.3.12)   [1주차를 보내며]    1주차     강의            Python                    파이썬 강의록 정리                       AIMath                    AIMath 강의록 정리                       Pytorch                    Pytorch 강의록 정리                           회고     ","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-1-1%EC%A3%BC%EC%B0%A8/",
+        "teaser": null
+      },{
+        "title": "Post: ai tech Day02",
+        "excerpt":"boostcamp_AITech_5th    ","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-2-day01/",
+        "teaser": null
+      },{
+        "title": "Post: Python 정리",
+        "excerpt":"jupyter: kernelspec: display_name: Python 3 language: python name: python3 language_info: codemirror_mode: name: ipython version: 3 file_extension: .py mimetype: text/x-python name: python nbconvert_exporter: python pygments_lexer: ipython3 version: 3.9.16 nbformat: 4 nbformat_minor: 0 — boostcamp_AITech_5th jupyter: kernelspec: display_name: Python 3 language: python name: python3 language_info: codemirror_mode: name: ipython version: 3 file_extension: .py...","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-1-python/",
+        "teaser": null
+      },{
+        "title": "Post: 딥러닝에 필요한 math 정리",
+        "excerpt":"boostcamp_AITech_5th 행렬곱 행과 열의 조건을 맞춰야 연산가능함. 역행렬 곱의 연산을 했을 때 항등행렬이 나온다. 유사 역행렬 무어펜로스(Moore-Penrose) 역행렬 np.linalg.pinv 행과 열의 크기에 따라 순서가 달라지니 유의 할 것. 연립방정식, 선형회귀 분석에 응용됨. 생각 왜 행렬을 배우는가? 차원이동, 연산 문제 사이킷런의 선형회귀 모델과 무어펜로즈의 선형회귀를 직접 구현해보시오. 딥러닝을 제대로 이해하기 위해선...","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-2-aimath/",
+        "teaser": null
+      },{
+        "title": "Post: 파이토치 정리 ",
+        "excerpt":"boostcamp_AITech_5th    ","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-3-day02/",
+        "teaser": null
+      },{
+        "title": "Post: 파이토치 정리 ",
+        "excerpt":"boostcamp_AITech_5th [Pytorch] 강의록 키워드 네트워크 구현 데이터 로딩 프로젝트 구조 잡기 로깅 Multi GPU 1. Introduction to PyTorch 밑바닥부터 딥러닝 코드 짜기? 책 딥러닝 프레임워크 텐서플로 파이토치 우리는 파이토치를 쓰자. 계산 그래프 (Computational Graph) Define and Rune 그래프를 먼저 정의 Define by Run 실행을 하면서 그래프를 생성 Pytorch 학회, 논문에...","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-3-pytorch/",
         "teaser": null
       }]
