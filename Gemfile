187540b3e2fd26c93683377573e0e20fea1360d4
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-08 11:41:54 +0900

    gemfile update

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
-gemspec
-gem "webrick", "~> 1.8"
+
+gem "jekyll"
+gem "minimal-mistakes-jekyll", :github => "mmistakes/minimal-mistakes"
+gem "kramdown-parser-gfm"
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,36 +8,42 @@ var store = [{
         "title": "Post: ai tech 1주차",
         "excerpt":"boostcamp_AITech_5th     (23.3.6 ~ 23.3.12)   [1주차를 보내며]    1주차     강의            Python                    파이썬 강의록 정리                       AIMath                    AIMath 강의록 정리                       Pytorch                    Pytorch 강의록 정리                           회고     ","categories": ["aitech"],
         "tags": [],
-        "url": "/aitech/post-1-1%EC%A3%BC%EC%B0%A8/",
+        "url": "/aitech/post-1%EC%A3%BC%EC%B0%A8/",
         "teaser": null
       },{
-        "title": "Post: ai tech Day02",
+        "title": "Post: ai tech - Day01",
         "excerpt":"boostcamp_AITech_5th    ","categories": ["aitech"],
         "tags": [],
-        "url": "/aitech/post-2-day01/",
-        "teaser": null
-      },{
-        "title": "Post: Python 정리",
-        "excerpt":"jupyter: kernelspec: display_name: Python 3 language: python name: python3 language_info: codemirror_mode: name: ipython version: 3 file_extension: .py mimetype: text/x-python name: python nbconvert_exporter: python pygments_lexer: ipython3 version: 3.9.16 nbformat: 4 nbformat_minor: 0 — boostcamp_AITech_5th jupyter: kernelspec: display_name: Python 3 language: python name: python3 language_info: codemirror_mode: name: ipython version: 3 file_extension: .py...","categories": ["aitech"],
-        "tags": [],
-        "url": "/aitech/post-1-python/",
+        "url": "/aitech/post-day01/",
         "teaser": null
       },{
         "title": "Post: 딥러닝에 필요한 math 정리",
         "excerpt":"boostcamp_AITech_5th 행렬곱 행과 열의 조건을 맞춰야 연산가능함. 역행렬 곱의 연산을 했을 때 항등행렬이 나온다. 유사 역행렬 무어펜로스(Moore-Penrose) 역행렬 np.linalg.pinv 행과 열의 크기에 따라 순서가 달라지니 유의 할 것. 연립방정식, 선형회귀 분석에 응용됨. 생각 왜 행렬을 배우는가? 차원이동, 연산 문제 사이킷런의 선형회귀 모델과 무어펜로즈의 선형회귀를 직접 구현해보시오. 딥러닝을 제대로 이해하기 위해선...","categories": ["aitech"],
         "tags": [],
-        "url": "/aitech/post-2-aimath/",
+        "url": "/aitech/post-aimath/",
         "teaser": null
       },{
-        "title": "Post: 파이토치 정리 ",
+        "title": "Post: ai tech - Day02 ",
         "excerpt":"boostcamp_AITech_5th    ","categories": ["aitech"],
         "tags": [],
-        "url": "/aitech/post-3-day02/",
+        "url": "/aitech/post-day02/",
         "teaser": null
       },{
-        "title": "Post: 파이토치 정리 ",
-        "excerpt":"boostcamp_AITech_5th [Pytorch] 강의록 키워드 네트워크 구현 데이터 로딩 프로젝트 구조 잡기 로깅 Multi GPU 1. Introduction to PyTorch 밑바닥부터 딥러닝 코드 짜기? 책 딥러닝 프레임워크 텐서플로 파이토치 우리는 파이토치를 쓰자. 계산 그래프 (Computational Graph) Define and Rune 그래프를 먼저 정의 Define by Run 실행을 하면서 그래프를 생성 Pytorch 학회, 논문에...","categories": ["aitech"],
+        "title": "Post: Python 정리",
+        "excerpt":"boostcamp_AITech_5th ::: {.cell .markdown collapsed=”false”} [파이썬] 강의 노트 [목차] 개요 기초 문법 객체 지향 파이썬으로 데이터 다루기 Numpy Pandas 개요 운영체제 윈도우, 맥, 리눅스 파일시스템 root 디렉토리에서 시작하는 트리 구조 디렉토리, 파일 절대경로, 상대경로 터미널 CLI 파이썬 플랫폼 독립적 인터프리터 언어 객체 지향 동적 타이핑 언어 컴파일러 VS 인터프리터 컴파일러...","categories": ["aitech"],
         "tags": [],
-        "url": "/aitech/post-3-pytorch/",
+        "url": "/aitech/post-python/",
+        "teaser": null
+      },{
+        "title": "Post: Pytorch ",
+        "excerpt":"1. Introduction to PyTorch 밑바닥부터 딥러닝 코드 짜기를 짤 수도 있습니다. 책 하지만 저희는 딥러닝 프레임워크를 사용합니다. 예를 들어서 텐서플로와 파이토치가 있습니다. 텐서플로는 구글에서 만들었고, 파이토치는 페이스북에서 만들었습니다. 저희는 파이토치를 사용할 것입니다. 파이토치는 장점이 꽤 많습니다. 대표적으로 Define by Run 이라는 작동 방식인데요. 이것은 실행을 한면서 계산 그래프(Computational Graph)를 생성하는...","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-pytorch/",
+        "teaser": null
+      },{
+        "title": "Post: ai tech -Day03 ",
+        "excerpt":"boostcamp_AITech_5th     d   d   dd  ","categories": ["aitech"],
+        "tags": [],
+        "url": "/aitech/post-day03/",
         "teaser": null
       }]
